988eaf88152c16b1b2da2812cb206d1ec615d707
Alex <NaudisGarcia51@gmail.com>
2023-02-20 16:10:32 -0400

    Inicio 17-proyecto

---
+++ 17-proyecto4/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <div className='layout'>
+      <h1>Proyecto React red social</h1>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ 17-proyecto4/src/main.jsx
@@ -0,0 +1,17 @@
+// Importaciones obligatoria de react 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+// Importar assets
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css'
+import './assets/css/normalize.css'
+import './assets/css/styles.css'
+import './assets/css/responsive.css'
+
+// Arrancar app de react
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    <App />
+  
+)
