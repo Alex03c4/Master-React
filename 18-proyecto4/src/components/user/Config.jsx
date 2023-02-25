5589e6ec2ae15d7cd9716a3c3e03b03f72fa3f8b
Alex <NaudisGarcia51@gmail.com>
2023-02-25 19:36:44 -0400

    boton de gente

--- 18-proyecto4/src/components/user/Config.jsx
+++ 18-proyecto4/src/components/user/Config.jsx
@@ -1,44 +1,72 @@
 import React, { useState } from "react";
 import { Global } from "../../helpers/Global";
 import avatar from "../../assets/img/user.png";
-import useAuth from '../../hooks/useAuth'
+import useAuth from "../../hooks/useAuth";
 import { SerializeForm } from "../../helpers/SerializeForm";
 export const Config = () => {
-	const {auth, setAuth} = useAuth()
-  const [saved, setSaved] = useState("not_saved")
+  const { auth, setAuth } = useAuth();
+  const [saved, setSaved] = useState("not_saved");
 
-  const updateUser = async(e)=> {
-    e.preventDefault()
-		//console.log(auth);
+  const updateUser = async (e) => {
+    e.preventDefault();
+    //console.log(auth);
 
-    // Recoger datos del formulario 
-    let newDataUser = SerializeForm(e.target)
+    const token = localStorage.getItem("token");
+
+    // Recoger datos del formulario
+    let newDataUser = SerializeForm(e.target);
 
     // Borrar propiedad innecesario
-    delete newDataUser.file0
+    delete newDataUser.file0;
 
-    // Actualizar usuario en la Bases de Datos 
+    // Actualizar usuario en la Bases de Datos
     const request = await fetch(Global.url + "user/update", {
       method: "PUT",
       body: JSON.stringify(newDataUser),
       headers: {
         "Content-Type": "application/json",
-        "Authorization": localStorage.getItem ("token")
-      }
-    })
+        Authorization: token,
+      },
+    });
 
-    const data = await request.json()  
-    if (data.status == "success") {
-      delete data.user.password
-      setAuth(data.user)
-      setSaved("saved")
-      
+    const data = await request.json();
+    if (data.status == "success" && data.user) {
+      delete data.user.password;
+      setAuth(data.user);
+      setSaved("saved");
     } else {
-      setSaved("error")
+      setSaved("error");
     }
 
+    //Subida de imagen
 
-  }
+    const fileInput = document.querySelector("#file");
+
+    if (data.status == "success" && fileInput.files[0]) {
+      // Recoger la imagen a subir
+      const formData = new FormData();
+      formData.append("file0", fileInput.files[0]); 
+
+      // Peticion para enviar el fichero
+      const uploadRequest = await fetch(Global.url + "user/upload", {
+        method: "POST",
+        body: formData,
+        headers: {          
+          Authorization: token,
+        }
+      });
+      const uploadData = await uploadRequest.json();
+
+      console.log(uploadData);
+      if (uploadData.status == "success" && uploadData.user) {
+        delete uploadData.user.password
+        setAuth(uploadData.user);
+        setSaved("saved");
+      } else {
+        setSaved("error");
+      }
+    }
+  };
   return (
     <>
       <header className="content__header content__header--public">
@@ -72,7 +100,7 @@ export const Config = () => {
 
           <div className="form-group">
             <label htmlFor="surname">Apellido</label>
-            <input type="text" name="surname"  defaultValue={auth.surname}/>
+            <input type="text" name="surname" defaultValue={auth.surname} />
           </div>
 
           <div className="form-group">
@@ -80,33 +108,51 @@ export const Config = () => {
             <input type="text" name="nick" defaultValue={auth.nick} />
           </div>
 
-					<div className="form-group">
+          <div className="form-group">
             <label htmlFor="bio">Bio</label>
-            <textarea name="bio" defaultValue={auth.bio}/>
+            <textarea name="bio" defaultValue={auth.bio} />
           </div>
 
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email"  defaultValue={auth.email}/>
+            <input type="email" name="email" defaultValue={auth.email} />
           </div>
 
           <div className="form-group">
             <label htmlFor="password">Contraseña</label>
-            <input type="password" name="password"  />
+            <input type="password" name="password" />
           </div>
-				
+
           <div className="form-group">
-						<label htmlFor="file0">Avatar</label>
-						<div className="general-info__container-avatar">
-                {auth.image !="default.png" && <img src={Global.url + "user/avatar/" + auth.image} className="container-avatar__img" alt="Foto de perfil" />}
-                {auth.image == "default.png" && <img src={avatar} className="container-avatar__img" alt="Foto de perfil" />}
+            <label htmlFor="file0">Avatar</label>
+            <div className="general-info__container-avatar">
+              {auth.image != "default.png" && (
+                <img
+                  src={Global.url + "user/avatar/" + auth.image}
+                  className="container-avatar__img"
+                  alt="Foto de perfil"
+                />
+              )}
+              {auth.image == "default.png" && (
+                <img
+                  src={avatar}
+                  className="container-avatar__img"
+                  alt="Foto de perfil"
+                />
+              )}
             </div>
-						<br />
-						<input type="file" name="file0" id="file" />
-        	</div> 
-					<br />     
-          <input type="submit" value="Actualizado" className="btn btn-success" />
+            <br />
+            <input type="file" name="file0" id="file" />
+          </div>
+          <br />
+          <input
+            type="submit"
+            value="Actualizado"
+            className="btn btn-success"
+          />
         </form>
+        <br />
+        <br />
       </div>
     </>
   );
--- 18-proyecto4/src/components/user/People.jsx
+++ 18-proyecto4/src/components/user/People.jsx
@@ -1,7 +1,108 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import avatar from "../../assets/img/user.png";
+import { Global } from "../../helpers/Global";
 export const People = () => {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [more, setMore] = useState(true)
+
+
+  useEffect(() => {
+    getUsers(1);
+  }, []);
+  const getUsers = async (nextPage = 1) => {
+    // Petición para sacar usuarios
+    const request = await fetch(Global.url + "user/list/" + nextPage, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+    });
+
+    const data = await request.json();
+
+    // Crear un estado para poder listar
+    if (data.users && data.status == "success") {
+
+      let newUsers = data.users
+
+      if(users.length >= 1) {
+        newUsers = [...users, ...data.users]
+      }
+      setUsers(newUsers);
+      
+      // paginacion
+      if (users.length >= data.total - data.users.length) {
+          setMore(false)
+      }
+      
+    }
+  };
+
+  const nextPage = () => {
+    let next = page + 1;
+    setPage(next);
+    getUsers(next);    
+  }
   return (
-    <div>People</div>
-  )
-}
+    <>
+      <header className="content__header">
+        <h1 className="content__title">Gente</h1>
+      </header>
+
+      <div className="content__posts">
+        {users.map((user) => {
+          return (
+            <article className="posts__post" key={user._id}>
+              <div className="post__container">
+                <div className="post__image-user">
+                  <a href="#" className="post__image-link">
+                  {user.image !="default.png" && <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Foto de perfil" />}
+                  {user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
+                        
+                  </a>
+                </div>
+
+                <div className="post__body">
+                  <div className="post__user-info">
+                    <a href="#" className="user-info__name">
+                      {user.name} {user.surname}
+                    </a>
+                    <span className="user-info__divider"> | </span>
+                    <a href="#" className="user-info__create-date">
+                      {user.created_at}
+                    </a>
+                  </div>
+
+                  <h4 className="post__content">{user.bio}</h4>
+                </div>
+              </div>
+
+              <div className="post__buttons">
+                <a href="#" className="post__button post__button--green">
+                  Seguir
+                </a>
+
+                <a href="#" className="post__button ">
+                  Dejar de seguir
+                </a>
+              </div>
+            </article>
+          );
+        })}
+        </div>
+          {/*genera un error*/}
+          {more &&         
+            <div className="content__container-btn">
+              <button className="content__btn-more-post" onClick={nextPage}>
+                ver mas persona
+              </button>
+            </div>
+          }  
+      
+
+      <br />
+    </>
+  );
+};
