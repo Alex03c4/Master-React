74ca9e75dbd29f6ab967c4042d19a10ac2e846be
Alex <NaudisGarcia51@gmail.com>
2023-03-06 14:45:41 -0400

    img publicacion

--- 18-proyecto4/src/components/user/Profile.jsx
+++ 18-proyecto4/src/components/user/Profile.jsx
@@ -124,6 +124,10 @@ export const Profile = () => {
       if (!newProfile &&  publications.length >= (data.total - data.publications.length)) {
         setMore(false)
       }
+
+      if (data.pages <= 1) {
+        setMore(false)
+      }
     }
 
   }
@@ -132,6 +136,25 @@ export const Profile = () => {
     let next = page + 1
     setPage(next)
     getPublications(next)
+  }
+
+  const deletePublication = async(publicationID) =>{
+    const request = await fetch(Global.url + "publication/remove/" + publicationID, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": localStorage.getItem("token"),
+      }
+    })
+
+    const data = await request.json()
+    setPage(1)
+    setMore(true)
+    getPublications(1,true)
+  
+
+
+
   }
 
   return (
@@ -229,14 +252,16 @@ export const Profile = () => {
                   </div>
 
                   <h4 className="post__content">{publication.text}</h4>
+
+                  {publication.file && <img src={Global.url + "publication/media/" + publication.file} />}
                 </div>
               </div>
 
               {auth._id == publication.user._id && 
                 <div className="post__buttons">
-                  <a href="#" className="post__button">
+                  <button onClick={() =>  deletePublication(publication._id)} className="post__button">
                     <i className="fa-solid fa-trash-can"></i>
-                  </a>
+                  </button>
                 </div>
               }
             </article>
